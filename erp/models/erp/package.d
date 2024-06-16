d23aa033d29aecd47e40365d6287ca51e2c2c455
UIMSolutions <ui.manufaktur@gmail.com>
2024-06-16 19:13:14 +0200

    Import from models

---
+++ erp/models/erp/package.d
@@ -0,0 +1,17 @@
+module models.erp;
+
+public import std.stdio;
+public import std.string;
+public import std.uuid;
+
+public import uim.core;
+public import uim.oop;
+public import uim.models;
+
+public import models.erp.attributes;
+public import models.erp.elements;
+public import models.erp.entities;
+public import models.erp.usecases;
+public import models.erp.values;
+
+public import models.erp.tests;
