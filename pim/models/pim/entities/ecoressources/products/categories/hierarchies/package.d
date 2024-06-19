4c01b67c6f1743c63c11f0e9de1ea96570dccd9d
UIMSolutions <ui.manufaktur@gmail.com>
2024-06-19 19:02:55 +0200

    Import to models-uim

---
+++ pim/models/pim/entities/ecoressources/products/categories/hierarchies/package.d
@@ -0,0 +1,6 @@
+
+module models.pim.ecoressources.products.categories.hierarchies;
+
+public import models.pim.ecoressources.products.categories.hierarchies.hierarchy;
+public import models.pim.ecoressources.products.categories.hierarchies.role;
+public import models.pim.ecoressources.products.categories.hierarchies.translation;
