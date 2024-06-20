0a944dca459ca446bbd110747f1bde10586e552e
UIMSolutions <ui.manufaktur@gmail.com>
2024-06-20 07:18:58 +0200

    Import to models-uim

---
+++ retail/models/retail/attributes/package.d
@@ -0,0 +1,7 @@
+module models.retail.attributes;
+
+public import models.retail.attributes.codes;
+public import models.retail.attributes.elements;
+public import models.retail.attributes.entites;
+public import models.retail.attributes.functions;
+public import models.retail.attributes.uuids;
---
+++ retail/models/retail/package.d
@@ -0,0 +1,20 @@
+module models.retail;
+
+// DLang libraries (Phobos)
+public import std.stdio;
+public import std.string;
+public import std.uuid;
+
+// External libraries
+public import vibe.d;
+
+// UIM libraries
+public import uim.core;
+public import uim.oop;
+public import uim.entities;
+
+// model packages
+public import models.retail.attributes;
+public import models.retail.classes;
+public import models.retail.elements;
+public import models.retail.entities;
